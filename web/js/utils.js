5d91779ffabba59d415429e935f8b5095f907d91
jost243 <80963334+a20erigasgar@users.noreply.github.com>
2022-11-29 12:01:22 +0100

    Prueba css i preguntas

--- web/js/utils.js
+++ web/js/utils.js
@@ -1,29 +1,49 @@
 const Home = Vue.component('home', {
-    template: `<div>hola</div>`
+  template: `<div>hola</div>`
 })
 
 const Partida = Vue.component('partida', {
-    template: `
-     
-    `
+  template: `<div class="slider">
+    <div class="slides">
+        <div id="slide-1">
+            <div class="container">
+                <div class="Pregunta">
+                    ¿Cómo le llaman los locales a la Ciudad de Nueva York?
+                </div>
+                <div class="Respuesta-1 incorrecta">
+                    <a href="#slide-2">World's City</a>
+                </div>
+                <div class="Respuesta-2 correcta">
+                    <a href="#slide-2">Gotham</a>
+                </div>
+                <div class="Respuesta-3 incorrecta">
+                    <a href="#slide-2">Mordor</a>
+                </div>
+                <div class="Respuesta-4 incorrecta">
+                    <a href="#slide-2">The city of freedom</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>`
 })
 
 // =============== Routes ===============
 const routes = [
-    {
-      path: "/",
-      component: Home,
-    },
-    {
-        path: "/joc",
-        component: Partida,
-    }
-  ];
-  
-  // 3. Create the router instance and pass the `routes` option
-  const router = new VueRouter({
-    routes, // short for `routes: routes`
-  });
+  {
+    path: "/",
+    component: Home,
+  },
+  {
+    path: "/joc",
+    component: Partida,
+  }
+];
+
+// 3. Create the router instance and pass the `routes` option
+const router = new VueRouter({
+  routes, // short for `routes: routes`
+});
 
 let app = new Vue({
   el: "#app",
