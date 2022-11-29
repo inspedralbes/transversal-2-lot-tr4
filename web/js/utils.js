b2f81e642640fe2179eb99b776b1fc7986bdf5b3
GasparGomez <a21gasgomtor@inspedralbes.cat>
2022-11-29 13:03:46 +0100

    proves fetch i targetes

--- web/js/utils.js
+++ web/js/utils.js
@@ -1,32 +1,49 @@
-const Home = Vue.component('home', {
-  template: `<div>hola</div>`
-})
+const Home = Vue.component("home", {
+  template: `<div>hola</div>`,
+});
 
-const Partida = Vue.component('partida', {
-  template: `<div class="slider">
-    <div class="slides">
+const Partida = Vue.component("partida", {
+  data: function () {
+    return {
+      datos: [],
+    };
+  },
+  template: `
+  <div class="slider">
+    <div class="slides" v-for="dada in datos">
         <div id="slide-1">
-            <div class="container">
+            <div class="container" >
                 <div class="Pregunta">
-                    ¿Cómo le llaman los locales a la Ciudad de Nueva York?
+                {{dada.question}}
                 </div>
                 <div class="Respuesta-1 incorrecta">
-                    <a href="#slide-2">World's City</a>
+                    <a href="#slide-2">{{dada.incorrectAnswers[0]}}</a>
                 </div>
                 <div class="Respuesta-2 correcta">
-                    <a href="#slide-2">Gotham</a>
+                    <a href="#slide-2">{{dada.correctAnswer}}</a>
                 </div>
                 <div class="Respuesta-3 incorrecta">
-                    <a href="#slide-2">Mordor</a>
+                    <a href="#slide-2">{{dada.incorrectAnswers[1]}}</a>
                 </div>
                 <div class="Respuesta-4 incorrecta">
-                    <a href="#slide-2">The city of freedom</a>
+                    <a href="#slide-2">{{dada.incorrectAnswers[2]}}</a>
                 </div>
             </div>
         </div>
     </div>
-</div>`
-})
+</div>`,
+  mounted: function () {
+    let url =
+      "https://the-trivia-api.com/api/questions?limit=10&difficulty=hard";
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        this.datos = data;
+      });
+  },
+  methods: {},
+});
 
 // =============== Routes ===============
 const routes = [
@@ -37,7 +54,7 @@ const routes = [
   {
     path: "/joc",
     component: Partida,
-  }
+  },
 ];
 
 // 3. Create the router instance and pass the `routes` option
