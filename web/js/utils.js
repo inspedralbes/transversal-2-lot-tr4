60afa79fc6b4d2549bff3f4c16734b0824a3f597
jost243 <80963334+a20erigasgar@users.noreply.github.com>
2022-11-30 12:25:18 +0100

    Respuestas y resultados

--- web/js/utils.js
+++ web/js/utils.js
@@ -11,29 +11,35 @@ const Partida = Vue.component("partida", {
     };
   },
   template: `
-  <div class="b-slider">
-    <div class="slider"> 
-      <div class="slides">
-          <div :id="'slide-' + (index)" v-for="(dada, index) in datos">
-              <div class="container" >
-                  <div class="Pregunta">
-                  {{dada.question}}
-                  </div>
-                  <div class="Respuesta-1 incorrecta" v-on:click="comprovaResultats('Resposta1-'+(index), dada.correctAnswer)">
-                      <a :id="'Resposta1-' + (index)" :href="'#slide-' + (index + 1)">{{dada.incorrectAnswers[0]}}</a>
-                  </div>
-                  <div class="Respuesta-2 correcta" v-on:click="comprovaResultats('Resposta2-'+(index), dada.correctAnswer)">
-                      <a :id="'Resposta2-' + (index)" :href="'#slide-' + (index + 1)">{{dada.correctAnswer}}</a>
-                  </div>
-                  <div class="Respuesta-3 incorrecta" v-on:click="comprovaResultats('Resposta3-'+(index), dada.correctAnswer)">
-                      <a :id="'Resposta3-' + (index)" :href="'#slide-' + (index + 1)">{{dada.incorrectAnswers[1]}}</a>
-                  </div>
-                  <div class="Respuesta-4 incorrecta" v-on:click="comprovaResultats('Resposta4-'+(index), dada.correctAnswer)">
-                      <a :id="'Resposta4-' + (index)" :href="'#slide-' + (index + 1)">{{dada.incorrectAnswers[2]}}</a>
-                  </div>
-              </div>
-          </div>
+  <div>
+    <div class="b-slider">
+      <div class="slider"> 
+        <div class="slides">
+            <div :id="'slide-' + (index)" v-for="(dada, index) in datos">
+                <div class="container" >
+                    <div class="Pregunta">
+                    {{dada.question}}
+                    </div>
+                    <div class="Respuesta-1 incorrecta" v-on:click="comprovaResultats('Resposta1-'+(index), dada.correctAnswer)">
+                        <a :id="'Resposta1-' + (index)" :href="'#slide-' + (index + 1)">{{dada.incorrectAnswers[0]}}</a>
+                    </div>
+                    <div class="Respuesta-2 correcta" v-on:click="comprovaResultats('Resposta2-'+(index), dada.correctAnswer)">
+                        <a :id="'Resposta2-' + (index)" :href="'#slide-' + (index + 1)">{{dada.correctAnswer}}</a>
+                    </div>
+                    <div class="Respuesta-3 incorrecta" v-on:click="comprovaResultats('Resposta3-'+(index), dada.correctAnswer)">
+                        <a :id="'Resposta3-' + (index)" :href="'#slide-' + (index + 1)">{{dada.incorrectAnswers[1]}}</a>
+                    </div>
+                    <div class="Respuesta-4 incorrecta" v-on:click="comprovaResultats('Resposta4-'+(index), dada.correctAnswer)">
+                        <a :id="'Resposta4-' + (index)" :href="'#slide-' + (index + 1)">{{dada.incorrectAnswers[2]}}</a>
+                    </div>
+                </div>
+            </div>
+        </div>
       </div>
+      <br>
+    </div>
+    <div id = "ResultsPrint">
+    </div>
   </div>`,
   mounted: function () {
     let url =
@@ -49,16 +55,15 @@ const Partida = Vue.component("partida", {
     comprovaResultats: function (respuestaUser, respuestaCorrecta) {
       let respuesta = document.getElementById(respuestaUser).innerHTML;
       if (respuesta == respuestaCorrecta) {
-        alert("RESPUESTA CORRECTA")
         this.contadorBuenas++;
+        document.getElementById("ResultsPrint").innerHTML = "<p>Correct Answer</p>"
       } else {
-        alert("RESPUESTA INCORRECTA")
         this.contadorMalas++;
-        console.log(this.contadorMalas)
-      } 
+        document.getElementById("ResultsPrint").innerHTML = "<p>Incorrect Answer</p>"
+      }
       if (this.contadorBuenas + this.contadorMalas == 10) {
-        alert("La teva puntuacio es " + this.contadorBuenas + "/10");
-      }      
+        document.getElementById("ResultsPrint").innerHTML = "<p>Your score is " + this.contadorBuenas + "/10</p>"
+      }
     }
   },
 });
